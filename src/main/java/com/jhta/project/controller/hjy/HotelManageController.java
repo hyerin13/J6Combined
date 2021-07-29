92ecbc91d394654b69a6df553aa59054f79376eb
H.gyoung <H.gyoung@DESKTOP-KHH7MR7>
2021-07-29 10:16:55 +0900

    지영-오류수정

--- src/main/java/com/jhta/project/controller/hjy/HotelManageController.java
+++ src/main/java/com/jhta/project/controller/hjy/HotelManageController.java
@@ -1,10 +1,11 @@
 package com.jhta.project.controller.hjy;
 
-import java.com.jhta.project.service.hjy.HotelManageService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.jhta.project.service.hjy.HotelManageService;
 
 
 @Controller
--- src/main/java/com/jhta/project/service/hjy/HotelManageService.java
+++ src/main/java/com/jhta/project/service/hjy/HotelManageService.java
@@ -1,9 +1,10 @@
 package com.jhta.project.service.hjy;
 
-import java.com.jhta.mybatis.mapper.hjy.HotelMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.jhta.mybatis.mapper.hjy.HotelMapper;
 
 
 @Service
