789f39a56cca30b315aab746c4edd11ba8d91885
user <user@172.16.11.210>
2021-08-03 14:25:39 +0900

    vo 추가

---
+++ src/main/java/com/jhta/project/vo/lhjcjy/SearchVo.java
@@ -0,0 +1,33 @@
+package com.jhta.project.vo.lhjcjy;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SearchVo {
+	private int aid;
+	private String aname;
+	private String aaddress;
+	private int atotalroom;
+	private String amainimg;
+	private int riid;
+	private int riminper;
+	private int rimaxper;
+	private int rireak;
+	private int risemipeak;
+	private int rioff;
+	private String rimainimg;
+	private String riextraimg1;
+	private String riextraimg2;
+	private int rid;
+	private Date rcheckin;
+	private Date rcheckout;
+	private int rexperson;
+	private int afid;
+	private int afpersonnel;
+}
