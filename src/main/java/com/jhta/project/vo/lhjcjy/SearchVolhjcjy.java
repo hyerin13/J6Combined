935202e8c2baeb2370064bfdbfe2c4cee3d7c9ab
user <user@172.16.11.210>
2021-08-11 09:20:16 +0900

    평점순정렬

--- src/main/java/com/jhta/project/vo/lhjcjy/SearchVolhjcjy.java
+++ src/main/java/com/jhta/project/vo/lhjcjy/SearchVolhjcjy.java
@@ -11,23 +11,46 @@ import lombok.NoArgsConstructor;
 @Data
 public class SearchVolhjcjy {
 	private int aid;
+	private String acate;
 	private String aname;
 	private String aaddress;
-	private int atotalroom;
+	private String aphone;
+	private String axcoordi;
+	private String aycoordi;
+	private String atotalroom;
+	private String agrade;
+	private String adetail;
 	private String amainimg;
-	private int riid;
-	private int riminper;
-	private int rimaxper;
-	private int rireak;
+	private Date aregdate;
+	private String aisdel;
+	
+	private String sum; //전체금액
+	private String amountSum; //평점순 전체금액
+	private String reday; //예약가능날짜
+	private int ripeak;
 	private int risemipeak;
 	private int rioff;
+
+	private int riid;
+	private String riroomtype;
+	private int riroom;
+	private String riservice;
+	private String risize;
+	private int riminper;
+	private int rimaxper;
 	private String rimainimg;
 	private String riextraimg1;
 	private String riextraimg2;
-	private int rid;
-	private Date rcheckin;
-	private Date rcheckout;
-	private int rexperson;
 	private int afid;
 	private int afpersonnel;
+	
+	private int peid;
+	private String peseason;
+	private Date pestart;
+	private Date peend;
+	
+	private int reid;
+	private int restar;
+	private int star;
+	private int rnum;
 }
