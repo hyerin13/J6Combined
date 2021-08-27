afb7d19ebfa1017185e7aeb6b6c02e3723d44093
H.gyoung <H.gyoung@DESKTOP-KHH7MR7>
2021-08-27 15:56:20 +0900

    지영 관리자 최종

--- src/main/webapp/admin/js/demo/index-demo.js
+++ src/main/webapp/admin/js/demo/index-demo.js
@@ -111,29 +111,5 @@ $.ajax({
          info: true,
    });
 
-//로그아웃태그클릭시 실행
-	$("#logoutAtag").click(function(){
-		$.ajax({
-				url:path+"hjy/admin/logout",
-				type:"get",
-				dataType:"json",
-				success:function(data){
-					location.href=path+"admin/login.html";
-				}
-		});
-	})
-   
-});
-$(document).ready(function() {
-	//아이디 불러오기
-	$.ajax({
-			url:path+"hjy/admin/getid",
-			data:{aid:$("#exampleInputEmail").val(),apw:$("#exampleInputPassword").val()},
-			type:"get",
-			dataType:"json",
-			success:function(data){
-				$("#adminid").html(data.adminid);
-			}
-	});
 })
 
